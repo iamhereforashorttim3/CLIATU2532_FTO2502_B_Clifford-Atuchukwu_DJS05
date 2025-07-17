67f8437d29a671a1c811bf3e06483e28d8b93aa8
Clifford <cliffordatuchukwu92@gmail.com>
2025-07-17 22:50:43 +0200

    Removed the App functions/grid from App.jsx into components for reusability and improving code structure

---
+++ src/Components/podcast-grid.jsx
@@ -0,0 +1,23 @@
+import { formatDistanceToNow } from "date-fns";
+import PodcastPreviews from "./podcastPreview";
+
+export default function PodcastGrid({ paginatedData, getGenres }) {
+  return (
+    <div className="podcast-grid">
+      {paginatedData.map((podcast) => (
+        <PodcastPreviews
+          key={podcast.id}
+          podcasts={{
+            img: podcast.image,
+            title: podcast.title,
+            seasons: podcast.seasons,
+            genres: getGenres(podcast.genres),
+            updated: formatDistanceToNow(new Date(podcast.updated), {
+              addSuffix: true,
+            }),
+          }}
+        />
+      ))}
+    </div>
+  );
+}
