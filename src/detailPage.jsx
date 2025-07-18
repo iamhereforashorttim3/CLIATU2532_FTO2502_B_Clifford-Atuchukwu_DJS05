8729d73026e1f26c28731c1379914b4da22048b9
Clifford <cliffordatuchukwu92@gmail.com>
2025-07-19 01:34:25 +0200

    Created the fetch function to dynamically fetch the ID with an error catcher and useEffect() for fetching the details on id change

--- src/detailPage.jsx
+++ src/detailPage.jsx
@@ -1,7 +1,40 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-export default function DetailPage() {
-  const [page, setPage] = useState([null]);
-  const [isLoading, setIsLoading] = useState([true]);
-  const [error, setError] = useState([null]);
+function DetailPage() {
+  const { id } = useParams();
+  const [show, setShow] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  async function fetchDetails() {
+    try {
+      const response = await fetch(`https://podcast-api.netlify.app/id/${id}`);
+      if (!response.ok) {
+        throw new Error("Failed to reach podcasts");
+      }
+      const data = await response.json();
+
+      setShow(data);
+      setError(null);
+    } catch (error) {
+      console.error("This did not work", error);
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchDetails();
+  }, [id]);
+  console.log("Shows", show);
+
+  return (
+    <>
+      <h1>This is text</h1>
+    </>
+  );
 }
+
+export default DetailPage;
