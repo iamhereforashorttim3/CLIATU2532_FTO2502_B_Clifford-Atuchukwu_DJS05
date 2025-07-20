92d65592c1116773d1d41857debd0f7c652282ae
Clifford <cliffordatuchukwu92@gmail.com>
2025-07-20 09:51:12 +0200

    Added JSdoc comments to major functions for clarity on it's purpose

--- src/detailPage.jsx
+++ src/detailPage.jsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import SeasonSelector from "./Components/Api/seasonSelector";
+import SeasonSelector from "./Components/utility/seasonSelector";
 import DetailPageContents from "./Components/detailPageContents";
 
+/**
+ * Renders the detail page for the specific podcast based on the ID of the URL.
+ * It handles fetching podcast data, managing loading and error states.
+ * Includes a select that allows users to choose which season to view.
+ *
+ * @returns {JSX.Element} The rendered podcast detail page
+ */
 function DetailPage() {
   const { id } = useParams();
   const [show, setShow] = useState();
@@ -10,6 +17,11 @@ function DetailPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  /**
+   * It fetches the podcast details from the API using the podcast "id"
+   * Updates the component state with the fetched data, if it fails an error message is displayed.
+   *
+   */
   async function fetchDetails() {
     try {
       const response = await fetch(`https://podcast-api.netlify.app/id/${id}`);
@@ -35,6 +47,9 @@ function DetailPage() {
     }
   }
 
+  /**
+   * It will re-fetch the details whenever the "id" changes
+   */
   useEffect(() => {
     fetchDetails();
   }, [id]);
